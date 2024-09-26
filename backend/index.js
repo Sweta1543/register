b449f85c784161e5b095aa6d6e9e34bff3b9d980
Sweta Singh <singhsweta1543@gmail.com>
2024-09-26 12:32:24 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('bodyParser');
+const cors = require('cors');
+const AuthRouter = require('./routes/AuthRouter');
+
+
+require('dotenv').config();
+require('./models/db');
+const PORT = process.env.PORT || 8080;
+app.get('/ping', (req, res) => {
+    res.send("PONG");
+})
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/auth', AuthRouter)
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`)
+})
---
+++ backend/routes/AuthRouter.js
@@ -0,0 +1,14 @@
+const { signup } = require('../controllers/AuthController');
+const { signupValidation } = require('../middlewares/AuthValidation');
+
+const router = require('express').Router;
+
+
+router.post('/login',(req, res) => {
+    res.send("login success");
+});
+
+router.post('/signup',signupValidation, signup);
+
+
+Module.exports = router;
